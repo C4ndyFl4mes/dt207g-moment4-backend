1cd9ca2dc06aecf5aa335bc8ec049d5e07427403
Isaac Svedin <swedinisaac@gmail.com>
2025-05-02 13:36:11 +0200

    Forgot that the passwords were to be hashed before storage. So I removed the password validation except for required.

--- models/accounts.js
+++ models/accounts.js
@@ -5,34 +5,12 @@ const accountSchema = new mongoose.Schema({
         type: String,
         required: true,
         minlength: 2,
-        maxlength: 32
+        maxlength: 32,
+        unique: true
     },
     password: {
         type: String,
-        required: true,
-        validate: {
-            validator: function(value) {
-                const errors = [];
-                if (value.length < 16) {
-                    errors.push("minst 16 tecken");
-                }
-                if (!/[A-Z]/.test(value)) {
-                    errors.push("minst en versal");
-                }
-                if (!/[a-z]/.test(value)) {
-                    errors.push("minst en gemen");
-                }
-                if (!/[!@#$%^&*]/.test(value)) {
-                    errors.push("minst ett specielt tecken (!@#$%^&*)");
-                }
-
-                if (errors.length > 0) {
-                    throw new Error(`Lösenordet måste ha: ${errors.join(", ")}`)
-                }
-
-                return true;
-            }
-        }
+        required: true
     },
     role: {
         type: String,
